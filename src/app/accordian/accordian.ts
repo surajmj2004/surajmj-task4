b5a41bd9a3a1ae12f77fe65d7c89d7598e71723b
surajmj2004 <suraj2622004@gmail.com>
2026-04-14 12:42:59 +0530

    Auto-generated commit

--- src/app/accordian/accordian.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-accordian',
-  imports: [],
-  templateUrl: './accordian.html',
-  styleUrl: './accordian.css',
-})
-export class Accordian {}
